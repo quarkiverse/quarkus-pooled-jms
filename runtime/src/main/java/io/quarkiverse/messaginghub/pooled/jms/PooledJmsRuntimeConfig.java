5bd0397718794eda00e5ec7ca5bf9fc10368d47a
Zheng Feng <zh.feng@gmail.com>
2024-08-21 15:36:12 +0800

    Swith to use @ConfigMapping

--- runtime/src/main/java/io/quarkiverse/messaginghub/pooled/jms/PooledJmsRuntimeConfig.java
+++ runtime/src/main/java/io/quarkiverse/messaginghub/pooled/jms/PooledJmsRuntimeConfig.java
@@ -1,46 +1,50 @@
 package io.quarkiverse.messaginghub.pooled.jms;
 
-import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
+import io.smallrye.config.WithName;
 
-@ConfigRoot(name = "pooled-jms", phase = ConfigPhase.RUN_TIME)
-public class PooledJmsRuntimeConfig {
+@ConfigRoot(phase = ConfigPhase.RUN_TIME)
+@ConfigMapping(prefix = "quarkus.pooled-jms")
+public interface PooledJmsRuntimeConfig {
 
     /**
      * Whether to enable pooling capabilities for JMS connections.
      */
-    @ConfigItem(name = "pooling.enabled", defaultValue = "true")
-    public boolean poolingEnabled;
+    @WithDefault("true")
+    @WithName("pooling.enabled")
+    boolean poolingEnabled();
 
     /**
      * Whether to enable {@link jakarta.jms.XAConnection} support and integrate with
      * {@link jakarta.transaction.TransactionManager}
      * If you enable it, you need to include `io.quarkus:quarkus-narayana-jta` extension.
      */
-    @ConfigItem(defaultValue = "disabled")
-    public TransactionIntegration transaction;
+    @WithDefault("disabled")
+    TransactionIntegration transaction();
 
     /**
      * Determines the maximum number of Connections the pool maintains in a single Connection pool (defaults to one).
      */
-    @ConfigItem(defaultValue = "1")
-    public int maxConnections;
+    @WithDefault("1")
+    int maxConnections();
 
     /**
      * The idle timeout (default 30 seconds) controls how long a Connection that hasn't been or currently isn't loaned
      * out to any client will remain idle in the Connection pool before it is eligible to be closed and discarded.
      * To disable idle timeouts the value should be set to 0 or a negative number.
      */
-    @ConfigItem(defaultValue = "30")
-    public int connectionIdleTimeout;
+    @WithDefault("30")
+    int connectionIdleTimeout();
 
     /**
      * used to establish a periodic check for expired Connections which will close all Connection that have exceeded
      * the set expiration value. This value is set to 0ms by default and only activates if set to a positive non-zero value.
      */
-    @ConfigItem(defaultValue = "0")
-    public long connectionCheckInterval;
+    @WithDefault("0")
+    long connectionCheckInterval();
 
     /**
      * by default the JMS pool will use it's own generic JMSContext classes to wrap a Connection borrowed from the pool
@@ -50,8 +54,8 @@ public class PooledJmsRuntimeConfig {
      * When enabled the JMSContext API is then not part of the Connections that are pooled by this JMS Connection pooling
      * library.
      */
-    @ConfigItem(defaultValue = "false")
-    public boolean useProviderJMSContext;
+    @WithDefault("false")
+    boolean useProviderJMSContext();
 
     /**
      * For each Connection in the pool there can be a configured maximum number of Sessions that the pooled Connection
@@ -59,8 +63,8 @@ public class PooledJmsRuntimeConfig {
      * meaning that each provider Connection is limited to 500 sessions, this limit can be disabled by setting the value
      * to a negative number.
      */
-    @ConfigItem(defaultValue = "500")
-    public int maxSessionsPerConnection;
+    @WithDefault("500")
+    int maxSessionsPerConnection();
 
     /**
      * When true (default) a call to createSession on a Connection from the pool will block until another previously
@@ -68,16 +72,16 @@ public class PooledJmsRuntimeConfig {
      * no Session is available will throw an IllegalStateException to indicate that the Connection is not able to provide
      * a new Session at that time.
      */
-    @ConfigItem(defaultValue = "true")
-    public boolean blockIfSessionPoolIsFull;
+    @WithDefault("true")
+    boolean blockIfSessionPoolIsFull();
 
     /**
      * When the blockIfSessionPoolIsFull option is enabled and this value is set then a call to createSession that has
      * blocked awaiting a Session will wait for the specified number of milliseconds before throwing an IllegalStateException.
      * By default this value is set to -1 indicating that the createSession call should block forever if configured to wait.
      */
-    @ConfigItem(defaultValue = "-1")
-    public int blockIfSessionPoolIsFullTimeout;
+    @WithDefault("-1")
+    int blockIfSessionPoolIsFullTimeout();
 
     /**
      * By default a Session that has been loaned out on a call to createSession will use a single anonymous JMS MessageProducer
@@ -86,6 +90,6 @@ public class PooledJmsRuntimeConfig {
      * MessageProducer
      * instance being created.
      */
-    @ConfigItem(defaultValue = "true")
-    public boolean useAnonymousProducers;
+    @WithDefault("true")
+    boolean useAnonymousProducers();
 }
